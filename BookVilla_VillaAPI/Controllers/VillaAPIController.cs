b2c3df19e56df672e6c571a9b5dfec590f84c50a
Bartosz Osmanski <bartosz.osmanski.external@eviden.com>
2024-06-21 14:07:12 +0200

    Added custom validation for POST endpoint

--- BookVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ BookVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -33,11 +33,15 @@ namespace BookVilla_VillaAPI.Controllers
         }
 
         [HttpPost]
-        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public ActionResult<VillaDTO> CreateVilla([FromBody] VillaDTO villaDTO)
         {
+            if (VillaStore.villaList.FirstOrDefault(u => u.Name.ToLower() == villaDTO.Name.ToLower()) != null)
+            {
+                ModelState.AddModelError("", "Villa already exists!");
+            }
             if (villaDTO == null)
             {
                 return BadRequest(villaDTO);
--- BookVilla_VillaAPI/Models/DTO/VillaDTO.cs
+++ BookVilla_VillaAPI/Models/DTO/VillaDTO.cs
@@ -1,8 +1,12 @@
-﻿namespace BookVilla_VillaAPI.Models.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookVilla_VillaAPI.Models.DTO
 {
     public class VillaDTO
     {
         public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
         public string Name { get; set; }
     }
 }
