f01879729ff34a629ae2920af58e3dfbea3ea166
Bartosz Osmanski <bartosz.osmanski.external@eviden.com>
2024-06-24 13:54:50 +0200

    Added HTTP put endpoint

--- BookVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ BookVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -76,5 +76,22 @@ namespace BookVilla_VillaAPI.Controllers
 
             return NoContent();
         }
+
+        [HttpPut("{id:int}", Name = "UpdateVilla")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult UpdateVilla(int id, [FromBody] VillaDTO villaDTO)
+        {
+            if (villaDTO == null || id != villaDTO.Id)
+            {
+                return BadRequest();
+            }
+            var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+            villa.Name = villaDTO.Name;
+            villa.Sqft = villaDTO.Sqft;
+            villa.Occupancy = villaDTO.Occupancy;
+
+            return NoContent();
+        }
     }
 }
