ddf6420e672074ef6c6ccbdf347637ffc52d3c5b
Bartosz Osmanski <bartosz.osmanski.external@eviden.com>
2024-06-26 11:43:23 +0200

    Added villa repository class

---
+++ BookVilla_VillaAPI/Repository/VillaRepository.cs
@@ -0,0 +1,62 @@
+﻿using BookVilla_VillaAPI.Data;
+using BookVilla_VillaAPI.Models;
+using BookVilla_VillaAPI.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace BookVilla_VillaAPI.Repository
+{
+    public class VillaRepository : IVillaRepository
+    {
+        private readonly ApplicationDBContext _db;
+
+        public VillaRepository(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+        public async Task Create(Villa entity)
+        {
+            await _db.Villas.AddAsync(entity);
+            await Save();
+        }
+
+        public async Task<Villa> Get(Expression<Func<Villa, bool>> filter = null, bool tracked = true)
+        {
+            IQueryable<Villa> query = _db.Villas;
+
+            if(!tracked)
+            {
+                query = query.AsNoTracking();
+            }
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+            return await query.FirstOrDefaultAsync();
+        }
+
+        public async Task<List<Villa>> GetAll(Expression<Func<Villa, bool>> filter = null)
+        {
+            IQueryable<Villa> query = _db.Villas;
+
+            if(filter != null)
+            {
+                query = query.Where(filter);
+            }
+            return await query.ToListAsync();
+        }
+
+        public async Task Remove(Villa entity)
+        {
+            _db.Villas.Remove(entity);
+            await Save();
+        }
+
+        public async Task Save()
+        {
+            await _db.SaveChangesAsync();
+        }
+    }
+}
