7bbb3fdaaaef5199c4781d1352c6505673621191
Bartosz Osmanski <bartosz.osmanski.external@eviden.com>
2024-07-01 11:34:43 +0200

    Added missing web project

---
+++ MagicVilla_Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BookVilla_Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
