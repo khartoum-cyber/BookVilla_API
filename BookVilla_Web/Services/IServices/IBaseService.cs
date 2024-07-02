18e265499dab975036507894c20e7b9f61273e0e
Bartosz Osmanski <bartosz.osmanski.external@eviden.com>
2024-07-02 11:36:10 +0200

    Created a service for consuming API

---
+++ BookVilla_Web/Services/IServices/IBaseService.cs
@@ -0,0 +1,10 @@
+﻿using BookVilla_Web.Models;
+
+namespace BookVilla_Web.Services.IServices
+{
+    public interface IBaseService
+    {
+        APIResponse responseModel { get; set; }
+        Task<T> SendAsync<T>(APIRequest aPIRequest);
+    }
+}
