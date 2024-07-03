f6086e6ca5f4a6e33ee07746122efc88fe720f52
Bartosz Osmanski <bartosz.osmanski.external@eviden.com>
2024-07-03 12:16:24 +0200

    Added villa controller with get all villas functionality

---
+++ BookVilla_Web/Controllers/VillaController.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using BookVilla_Web.Models;
+using BookVilla_Web.Models.DTO;
+using BookVilla_Web.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace BookVilla_Web.Controllers
+{
+    public class VillaController : Controller
+    {
+        private readonly IVillaService _villaService;
+        private readonly IMapper _mapper;
+
+        public VillaController(IVillaService villaService, IMapper mapper)
+        {
+            _villaService = villaService;
+            _mapper = mapper;
+        }
+
+        public async Task<IActionResult> IndexVilla()
+        {
+            List<VillaDTO> list = new();
+
+            var response = await _villaService.GetAllAsync<APIResponse>();
+            if (response != null && response.IsSuccess)
+            {
+                list = JsonConvert.DeserializeObject<List<VillaDTO>>(Convert.ToString(response.Result));
+            }
+            return View(list);
+        }
+    }
+}
