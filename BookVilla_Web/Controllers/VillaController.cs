8f51f9526673fce4a4a278e2dcac92219bc4a954
Bartosz Osmanski <bartosz.osmanski.external@eviden.com>
2024-07-05 11:24:09 +0200

    Update villa action method

--- BookVilla_Web/Controllers/VillaController.cs
+++ BookVilla_Web/Controllers/VillaController.cs
@@ -4,6 +4,7 @@ using BookVilla_Web.Models.DTO;
 using BookVilla_Web.Services.IServices;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.Reflection;
 
 namespace BookVilla_Web.Controllers
 {
@@ -47,6 +48,32 @@ namespace BookVilla_Web.Controllers
                     return RedirectToAction(nameof(IndexVilla));
                 }
             }
+			return View(model);
+		}
+
+		public async Task<IActionResult> UpdateVilla(int villaId)
+		{
+			var response = await _villaService.GetAsync<APIResponse>(villaId);
+			if (response != null && response.IsSuccess)
+			{
+				VillaDTO model = JsonConvert.DeserializeObject<VillaDTO>(Convert.ToString(response.Result));
+				return View(_mapper.Map<VillaDTOupdate>(model));
+			}
+			return NotFound();
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> UpdateVilla(VillaDTOupdate model)
+		{
+			if (ModelState.IsValid)
+			{
+				var response = await _villaService.UpdateAsync<APIResponse>(model);
+				if (response != null && response.IsSuccess)
+				{
+					return RedirectToAction(nameof(IndexVilla));
+				}
+			}
 			return View(model);
 		}
 	}
