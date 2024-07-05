55f2f5a6973c1e0f57930f1736b1eca63bf51861
Bartosz Osmanski <bartosz.osmanski.external@eviden.com>
2024-07-05 12:03:40 +0200

    Added villa number service

---
+++ BookVilla_Web/Controllers/VillaNumberController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookVilla_Web.Controllers
+{
+    public class VillaNumberController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ BookVilla_Web/Services/IServices/IVillaNumberService.cs
@@ -0,0 +1,13 @@
+﻿using BookVilla_Web.Models.DTO;
+
+namespace BookVilla_Web.Services.IServices
+{
+    public interface IVillaNumberService
+    {
+        Task<T> GetAllAsync<T>();
+        Task<T> GetAsync<T>(int id);
+        Task<T> CreateAsync<T>(VillaNumberDTOcreate dto);
+        Task<T> UpdateAsync<T>(VillaNumberDTOupdate dto);
+        Task<T> DeleteAsync<T>(int id);
+    }
+}
