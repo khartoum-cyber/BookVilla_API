24c8eee2a28cc9d2dac3274803361309ad9c8a09
Bartosz Osmanski <bartosz.osmanski.external@eviden.com>
2024-07-08 14:28:04 +0200

    Created IndexVillaNumber for getting list of villa numbers in VillaNumber controller

--- BookVilla_Web/Controllers/VillaNumberController.cs
+++ BookVilla_Web/Controllers/VillaNumberController.cs
@@ -1,12 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using BookVilla_Web.Models;
+using BookVilla_Web.Models.DTO;
+using BookVilla_Web.Services;
+using BookVilla_Web.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace BookVilla_Web.Controllers
 {
     public class VillaNumberController : Controller
     {
-        public IActionResult Index()
+        private readonly IVillaNumberService _villaNumberService;
+        private readonly IMapper _mapper;
+        public VillaNumberController(IVillaNumberService villaNumberService, IMapper mapper)
         {
-            return View();
+            _villaNumberService = villaNumberService;
+            _mapper = mapper;
+        }
+        public async Task<IActionResult> IndexVillaNumber()
+        {
+            List<VillaNumberDTO> list = new();
+
+            var response = await _villaNumberService.GetAllAsync<APIResponse>();
+            if (response != null && response.IsSuccess)
+            {
+                list = JsonConvert.DeserializeObject<List<VillaNumberDTO>>(Convert.ToString(response.Result));
+            }
+            return View(list);
         }
     }
 }
