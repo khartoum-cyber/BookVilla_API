662a8806806949b17f2f9ee2d8cf6b8534fb5717
Bartosz Osmanski <bartosz.osmanski.external@eviden.com>
2024-07-10 11:38:25 +0200

    Added view models for delete and update actions

---
+++ BookVilla_Web/Models/ViewModel/VillaNumberDeleteViewModel.cs
@@ -0,0 +1,18 @@
+﻿using BookVilla_Web.Models.DTO;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BookVilla_Web.Models.ViewModel
+{
+    public class VillaNumberDeleteViewModel
+    {
+        public VillaNumberDeleteViewModel()
+        {
+            VillaNumber = new VillaNumberDTO();
+        }
+
+        public VillaNumberDTO VillaNumber { get; set; }
+        [ValidateNever]
+        public IEnumerable<SelectListItem> VillaList { get; set; }
+    }
+}
---
+++ BookVilla_Web/Models/ViewModel/VillaNumberUpdateViewModel.cs
@@ -0,0 +1,18 @@
+﻿using BookVilla_Web.Models.DTO;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BookVilla_Web.Models.ViewModel
+{
+    public class VillaNumberUpdateViewModel
+    {
+        public VillaNumberUpdateViewModel()
+        {
+            VillaNumber = new VillaNumberDTOupdate();
+        }
+
+        public VillaNumberDTOupdate VillaNumber { get; set; }
+        [ValidateNever]
+        public IEnumerable<SelectListItem> VillaList { get; set; }
+    }
+}
