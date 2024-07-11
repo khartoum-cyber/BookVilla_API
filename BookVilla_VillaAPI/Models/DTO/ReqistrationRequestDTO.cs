917fccf46c93a98c9f250f8dc8e91688730022b0
methkill <methkill@gmail.com>
2024-07-11 10:25:09 +0200

    DTO's for login and registration purposes

---
+++ BookVilla_VillaAPI/Models/DTO/ReqistrationRequestDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace BookVilla_VillaAPI.Models.DTO
+{
+    public class ReqistrationRequestDTO
+    {
+        public string UserName { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+    }
+}
