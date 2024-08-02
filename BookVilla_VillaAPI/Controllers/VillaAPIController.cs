4ffcb4f4a8bf7eb1d035cfe7ed12abba242e6b19
Bartosz Osmanski <bartosz.osmanski.external@eviden.com>
2024-08-02 11:30:15 +0200

    Added status code attributes for villa api methods

--- BookVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ BookVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -30,6 +30,8 @@ namespace BookVilla_VillaAPI.Controllers
         [HttpGet]
         [Authorize]
         [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
         public async Task<ActionResult<APIResponse>> GetVillas()
         {
             try
@@ -51,6 +53,8 @@ namespace BookVilla_VillaAPI.Controllers
         [HttpGet("{id:int}", Name = "GetVilla")]
         [Authorize(Roles = "admin")]
         [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<APIResponse>> GetVilla(int id)
@@ -117,6 +121,8 @@ namespace BookVilla_VillaAPI.Controllers
         [HttpDelete("{id:int}", Name = "DeleteVilla")]
         [Authorize(Roles = "CUSTOM")]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<APIResponse>> DeleteVilla(int id)
