e31ec53f75f297604f260ae2e4e28bd526539512
Bartosz Osmanski <bartosz.osmanski.external@eviden.com>
2024-08-05 11:33:31 +0200

    Added DTO's for login and registration

---
+++ BookVilla_Web/Models/DTO/RegistrationRequestDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace BookVilla_Web.Models.DTO
+{
+    public class RegistrationRequestDTO
+    {
+        public string UserName { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+    }
+}
