6ba3d3b43c35fc98d49db3ba66379bd09a3a712b
Bartosz Osmanski <bartosz.osmanski.external@eviden.com>
2024-08-05 11:40:07 +0200

    auth service interface

---
+++ BookVilla_Web/Services/IServices/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using BookVilla_Web.Models.DTO;
+
+namespace BookVilla_Web.Services.IServices
+{
+    public interface IAuthService
+    {
+        Task<T> LoginAsync<T>(LoginRequestDTO objToCreate);
+        Task<T> RegisterAsync<T>(UserDTO objToCreate);
+    }
+}
