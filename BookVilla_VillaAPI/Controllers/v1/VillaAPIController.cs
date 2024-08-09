4794681db63de5e90dfeb03e099a869e959c0ad8
Bartosz Osmanski <bartosz.osmanski.external@eviden.com>
2024-08-09 11:49:56 +0200

    Filtering by name

--- BookVilla_VillaAPI/Controllers/v1/VillaAPIController.cs
+++ BookVilla_VillaAPI/Controllers/v1/VillaAPIController.cs
@@ -33,7 +33,7 @@ namespace BookVilla_VillaAPI.Controllers.v1
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status403Forbidden)]
-        public async Task<ActionResult<APIResponse>> GetVillas([FromQuery(Name = "filterOccupancy")] int? occupancy)
+        public async Task<ActionResult<APIResponse>> GetVillas([FromQuery(Name = "filterOccupancy")] int? occupancy, [FromQuery] string? search)
         {
             try
             {
@@ -46,6 +46,11 @@ namespace BookVilla_VillaAPI.Controllers.v1
                 else
                 {
                     villaList = await _repo.GetAllAsync();
+                }
+
+                if (!string.IsNullOrEmpty(search))
+                {
+                    villaList = villaList.Where(u => u.Name.ToLower().Contains(search));
                 }
 
                 _logger.LogInformation("Getting all villas.");
