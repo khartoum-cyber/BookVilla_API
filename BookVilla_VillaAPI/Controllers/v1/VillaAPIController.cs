cbc9a7091f27fdf31e136223543aa23121297774
Bartosz Osmanski <bartosz.osmanski.external@eviden.com>
2024-08-09 11:42:44 +0200

    Added basic filter

--- BookVilla_VillaAPI/Controllers/v1/VillaAPIController.cs
+++ BookVilla_VillaAPI/Controllers/v1/VillaAPIController.cs
@@ -33,12 +33,22 @@ namespace BookVilla_VillaAPI.Controllers.v1
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status403Forbidden)]
-        public async Task<ActionResult<APIResponse>> GetVillas()
+        public async Task<ActionResult<APIResponse>> GetVillas([FromQuery(Name = "filterOccupancy")] int? occupancy)
         {
             try
             {
+                IEnumerable<Villa> villaList;
+
+                if (occupancy > 0)
+                {
+                    villaList = await _repo.GetAllAsync(u => u.Occupancy == occupancy);
+                }
+                else
+                {
+                    villaList = await _repo.GetAllAsync();
+                }
+
                 _logger.LogInformation("Getting all villas.");
-                var villaList = await _repo.GetAllAsync();
                 _response.Result = _mapper.Map<List<VillaDTO>>(villaList);
                 _response.StatusCode = HttpStatusCode.OK;
                 return Ok(_response);
